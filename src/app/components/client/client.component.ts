bc261751c1f205f5ff07d834e5e9b6b61409cc89
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2025-04-19 18:51:49 -0300

    feat: Chamada de funções, chamada de apis

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Client } from '../../model/class/Client';
+import { FormsModule } from '@angular/forms';
+import { ClientService } from '../service/client.service';
+import { ApiResponseModel } from '../../model/class/interface/role';
 
 @Component({
   selector: 'app-client',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit {
 
+  clientService = inject(ClientService);
+  clientObject: Client = new Client();
+  clientList: Client[] = [];
+
+  ngOnInit(): void {
+    this.loadClient();
+  }
+
+  loadClient(){
+    this.clientService.getAllClients().subscribe((data:ApiResponseModel) => {
+      if(data.result){
+        this.clientList = data.data;
+      }
+    });
+  }
+
+  onSaveClient(){
+    this.clientService.addUpdate(this.clientObject).subscribe((res:ApiResponseModel) => {
+      if(res.result){
+       alert("Client created successfully");
+       this.loadClient();
+       this.clientObject = new Client();
+      }else{
+        alert("Client creation failed");
+      }
+    });
+  }
+  onDelete(id: number){
+    
+    const isDelete = confirm("Are you sure to delete this client?");
+    if(isDelete){
+      this.clientService.deleteClientById(id).subscribe((res:ApiResponseModel) => {
+        if(res.result){
+          alert("Client Deleted successfully");
+          this.loadClient();
+        }else{
+          alert(res.message);
+        }
+      });
+    }
+  }
+
+  onEdit(client: Client){
+    this.clientObject = client;
+  }
+  onResetForm(){
+    this.clientObject = new Client();
+  }
 }
---
+++ src/app/components/service/client.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Client } from '../../model/class/Client';
+import { ApiResponseModel } from '../../model/class/interface/role';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http: HttpClient) {
+   }
+
+   getAllClients():Observable<ApiResponseModel>{
+    return this
+            .http
+            .get<ApiResponseModel>(environment.API_CLIENT_URL+"GetAllClients");
+   }
+   addUpdate(obj:Client):Observable<ApiResponseModel>{
+    return this
+            .http
+            .post<ApiResponseModel>(environment.API_CLIENT_URL+"AddUpdateClient", obj);
+   }
+   deleteClientById(id:number):Observable<ApiResponseModel>{
+    return this
+            .http
+            .delete<ApiResponseModel>(environment.API_CLIENT_URL +"DeleteClientByClientId?clientId="+id);
+   }
+
+}
