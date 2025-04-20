1633fa259b01d3674768724cd80d14f2114a28ff
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2025-04-19 21:28:26 -0300

    feat: validação de forms (validar se o conteúdo de um determinado campo está condizente com o esperado e também validar se o forms está pronto para ser enviado através da habilitação ou não do botão de submissão do forms

---
+++ src/app/components/components/client-project/client-project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-client-project',
+  imports: [],
+  templateUrl: './client-project.component.html',
+  styleUrl: './client-project.component.css'
+})
+export class ClientProjectComponent {
+  
+}
