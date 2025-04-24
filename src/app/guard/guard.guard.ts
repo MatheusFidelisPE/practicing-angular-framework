ed195107935e748e20fe6cce9a2d70c0189b2beb
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2025-04-23 22:13:19 -0300

    feat: carrossel no angular

--- src/app/guard/guard.guard.ts
+++ src/app/guard/guard.guard.ts
@@ -3,8 +3,8 @@ import { CanActivateFn, Router } from '@angular/router';
 
 export const guardGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const localUser = localStorage.getItem('empErpUser');
-
+  const localUser = localStorage.getItem('angular18');
+  debugger;
   if (localUser != null) {
     return true;
   }else {
