c2f9d77b76164bf238cfe554d72c263889a73c12
matheus_fidelis <mfds@a.recife.ifpe.edu.br>
2025-04-29 14:04:08 -0300

    feat: criando forms filtro

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,36 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { ITodoList } from '../../model/class/interface/role';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DeferBlockBehavior } from '@angular/core/testing';
+
 
 @Component({
   selector: 'app-todolist',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './todolist.component.html',
   styleUrl: './todolist.component.css'
 })
 export class TodolistComponent implements OnInit{
-  
+
   realTodolist : ITodoList[] = [];
   todoList : ITodoList[] = [];
+  dateFilterForm:any;
+
   todoItem = {
     status:false,
     description:'', 
     realizationDate:new Date()
     };
   countValue: number = 0; 
+  constructor(private formBuilder:FormBuilder){}
   ngOnInit(): void {
     var tasksString = localStorage.getItem('tasks');
     if(tasksString != null){
       this.realTodolist = JSON.parse(tasksString);
       this.todoList = this.realTodolist;
     }
-  
+    this.dateFilterForm = this.formBuilder.group({
+      selectedDate:['']
+      }
+    );
   }
   onSubmit(todoForm:any):void{
     console.log('entrou no submit');
     this.realTodolist.push(this.todoItem);
     localStorage.setItem('tasks', JSON.stringify(this.realTodolist));
+    this.todoList.push(this.todoItem);
     this.todoItem = {
       status:false, 
       description: '', 
@@ -43,7 +52,31 @@ export class TodolistComponent implements OnInit{
   showAllTasks(){
     this.todoList = this.realTodolist;
   }
+  filterByOpen(){
+    this.todoList = this.realTodolist.filter(x => !x.status);
+  }
   updateList(){
     localStorage.setItem('tasks', JSON.stringify(this.realTodolist));
   }
+  onFilterDate(dateFilterForm:FormGroup){
+    debugger;
+    const selectDateStr = dateFilterForm.value.selectedDate;
+    console.log('data string'+selectDateStr);
+    const selectedDate = new Date(selectDateStr);
+    console.log(selectedDate.getFullYear());
+    console.log(selectedDate.getMonth());
+    console.log(selectedDate.getDate());
+    this.todoList = this
+        .realTodolist
+        .filter(
+          item => { 
+            const itemDate = item.realizationDate;
+            debugger;
+            return itemDate.getFullYear() === selectedDate.getFullYear() &&
+                   itemDate.getMonth() === selectedDate.getMonth() &&
+                   itemDate.getDate() === selectedDate.getDate();
+            
+          });
+  }
+
 }
